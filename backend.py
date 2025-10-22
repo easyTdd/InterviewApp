f93348c21a51d35051bcaec8f2b32c42ace41f0a
kazys <kazys.rackauskas@gmail.com>
2025-10-22 10:48:07 +0300

    some more promt improvements. UI improvements

--- backend.py
+++ backend.py
@@ -10,20 +10,47 @@ client = OpenAI(api_key=OPENAI_API_KEY)
 
 prompts = [
     {
-        "role": "system", 
         "content": "You are a helpful interview preparation assistant. You are interviewing the user for a .NET Software Developer position. The user is shown a greeting at the beginning of the conversation. Assume you have already said: “Hi, it’s great to meet you. I’m glad you could make it today. Before we begin, how are you feeling?” The first user response will be an answer to this question. In the background, think of the top 10 questions for this interview and ask them one by one. When the user answers, write feedback in italics, then proceed with the next question. After all 10 questions, thank the user and conclude the interview."
-    }
+    },
+    {
+        "content": "You are a helpful interview preparation assistant. You are interviewing the user for a .NET Software Developer position. The user is shown a greeting at the beginning of the conversation. Assume you have already said: “Hi, it’s great to meet you. I’m glad you could make it today. Before we begin, how are you feeling?” The first user response will be an answer to this question. "
+        "In the background, think of the top 10 questions for this interview and ask them one by one. "
+        "When the user answers evaluate if the interviewer could be encouraged to expand on their answer by asking follow-up questions. These follow-up questions doesn't count towards the 10 questions."
+        "In italic provide feedback on the user's answer, pointing mistakes, suggesting improvements or praising strong points. This part is not assumed as a part of the conversation with the user. "
+        "After all 10 questions, thank the user and conclude the interview."
+    },
+    {
+        "content": "You are a helpful interview preparation assistant. You are interviewing the user for a .NET Software Developer position. The user is shown a greeting at the beginning of the conversation. Assume you have already said: “Hi, it’s great to meet you. I’m glad you could make it today. Before we begin, how are you feeling?” The first user response will be an answer to this question. "
+        "In the background, think of the top 10 questions for this interview and ask them one by one. "
+        "When the user answers evaluate if the interviewer could be encouraged to expand on their answer by asking follow-up questions. These follow-up questions doesn't count towards the 10 questions."
+        "In italic provide feedback on the user's answer, pointing mistakes, suggesting improvements or praising strong points. This part is not assumed as a part of the conversation with the user. "
+        "After all 10 questions, thank the user and conclude the interview."
+    },
+    {
+        "content": "You are a helpful interview preparation assistant. You are interviewing the user for a {position} position in {industry} industry/company. The user is shown a greeting at the beginning of the conversation. Assume you have already said: “Hi, it’s great to meet you. I’m glad you could make it today. Before we begin, how are you feeling?” The first user response will be an answer to this question. "
+        "In the background, think of the top {number_of_questions} questions for this interview and ask them one by one. "
+        "When the user answers evaluate if the interviewer could be encouraged to expand on their answer by asking follow-up questions. These follow-up questions doesn't count towards the {number_of_questions} questions."
+        "In italic provide feedback on the user's answer, pointing mistakes, suggesting improvements or praising strong points. Do not pay attention at grammar mistakes. This part is not assumed as a part of the conversation with the user. "
+        "After all {number_of_questions} questions, thank the user and conclude the interview."
+    },
+
 ];
 
 def get_openai_response(
         history,
-        temperature=0.7):
-    fullInput = [prompts[-1]] + history
+        input):
+    
+    systemPrompt = {
+        "role": "system", 
+        "content": prompts[-1]["content"].format(**input)
+    }
+
+    fullInput = [systemPrompt] + history
 
     response = client.responses.create(
         model="gpt-4.1",
         input=fullInput,
-        temperature=temperature
+        temperature=input["temperature"]
     )
 
     return response.output_text
---
+++ front.py
@@ -0,0 +1,74 @@
+
+import streamlit as st
+from backend import get_openai_response
+
+def on_user_input_change():
+    if(st.session_state.user_input.strip() == ""):
+        return
+    
+    st.session_state.chat_history.append({"role": "user", "content": st.session_state.user_input})
+
+    response = get_openai_response(
+        st.session_state.chat_history,
+        input=st.session_state.input
+    )
+
+    st.session_state.chat_history.append({"role": "assistant", "content": response})
+    st.session_state.user_input = ""
+
+def on_interview_start():
+    st.write(f"Temperature {st.session_state.temperature}, Number of questions {st.session_state.number_of_questions}, Position {st.session_state.position}, Industry {st.session_state.industry}")
+    st.session_state.chat_history = []
+    st.session_state.in_conversation = True
+    st.session_state.user_input = ""
+    st.session_state.input = {
+        "temperature": st.session_state.temperature,
+        "position": st.session_state.position,
+        "industry": st.session_state.industry,
+        "number_of_questions": st.session_state.number_of_questions
+    }
+
+def on_interview_end():
+    st.session_state.in_conversation = False
+    st.session_state.user_input = ""
+    st.session_state.input = None
+
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
+
+if "clear_input" not in st.session_state:
+    st.session_state.clear_input = False
+
+if "in_conversation" not in st.session_state:
+    st.session_state.in_conversation = False
+
+st.title("Interview Chatbot")
+
+if st.session_state.in_conversation == False:
+    st.slider('Temperature', min_value=0.0, max_value=2.0, value=0.7, step=0.1, key="temperature")
+    st.slider('Number of questions', min_value=1, max_value=10, value=3, step=1, key="number_of_questions")
+    st.text_input("What position are you interviewing for?", key="position")
+    st.text_input("What industry/company is the job in?", key="industry")
+    st.button("Start Interview", on_click=on_interview_start)
+    st.write(f"Temperature {st.session_state.temperature}, Number of questions {st.session_state.number_of_questions}, Position {st.session_state.position}, Industry {st.session_state.industry}")
+
+else:    
+    st.button("Start new interview", on_click=on_interview_end)
+    st.markdown(f"**Interviewer:** Hi, it’s great to meet you. I’m glad you could make it today. Before we begin, how are you feeling?")
+
+    for msg in st.session_state.chat_history:
+        role_display = "Interviewer" if msg['role'] == "assistant" else "You"
+        st.markdown(f"**{role_display}:** {msg['content']}")
+
+    st.text_area("Type your message...", key="user_input", on_change=on_user_input_change)
+
+    st.button("Send", on_click=on_user_input_change)
+
+    st.markdown(
+        """
+        <script>
+        document.querySelector('div.st-key-user_input input').focus();
+        </script>
+        """,
+        unsafe_allow_html=True
+    )
